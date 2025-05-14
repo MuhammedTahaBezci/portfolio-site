863de270af6a8f5ef913d6b107cf7a12b7e6ec46
tahabezci <bezcitaha@gmail.com>
2025-05-14 19:41:23 +0300

    cors hatası düzeltilmedi

--- src/types/exhibition.ts
+++ src/types/exhibition.ts
@@ -1,12 +1,22 @@
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
 
 export interface Exhibition {
-    endDate: string | number | Date;
-    startDate: string | number | Date;
-    imageUrl: string | StaticImport;
-    id: string;
-    title: string;
-    date: string;
-    location: string;
-    description?: string;
-  }
+  id: string;
+  title: string;
+  startDate: string | number | Date;
+  endDate: string | number | Date;
+  date: string;
+  imageUrl: string | StaticImport;
+  location: string;
+  description?: string;
+  galleryUrl?: string;
+  galleryName?: string;
+}
+
+export function formatDate(date: string | number | Date) {
+  return new Intl.DateTimeFormat('tr-TR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }).format(new Date(date));
+}
